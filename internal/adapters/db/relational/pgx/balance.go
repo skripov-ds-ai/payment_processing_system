261582ea6d0e2be091fbc8b2524dc8d5f14932f3
Denis Skripov <nizhikebinesi@gmail.com>
2023-01-23 22:43:07 +0600

    add decrease balance

--- internal/adapters/db/relational/pgx/balance.go
+++ internal/adapters/db/relational/pgx/balance.go
@@ -3,10 +3,11 @@ package pgx
 import (
 	"context"
 	"fmt"
+	"payment_processing_system/internal/domain/entity"
+
 	sq "github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/labstack/gommon/log"
-	"payment_processing_system/internal/domain/entity"
 )
 
 type balanceStorage struct {
@@ -25,15 +26,14 @@ func NewBalanceStorage(pool *pgxpool.Pool) *balanceStorage {
 }
 
 func (bs *balanceStorage) IncreaseAmount(ctx context.Context, id string, amount int64) error {
-	column := fmt.Sprintf("%s.%s", bs.tableScheme, "amount")
-	onConflict := fmt.Sprintf("ON CONFLICT DO UPDATE SET %s = %s + ?", column, column)
+	onConflict := "ON CONFLICT DO UPDATE SET amount = amount + ?"
 	sql, args, buildErr := bs.queryBuilder.
 		Insert(bs.tableScheme).Columns("id", "amount").
 		Values(id, amount).Suffix(onConflict, amount).ToSql()
 	log.Info(fmt.Sprintf("table = %q ; sql = %q ; args = %q", bs.tableScheme, sql, args))
 	if buildErr != nil {
 		// TODO: add wrapping
-		//buildErr
+		// buildErr
 		return buildErr
 	}
 	if exec, execErr := bs.pool.Exec(ctx, sql, args...); execErr != nil {
@@ -46,16 +46,36 @@ func (bs *balanceStorage) IncreaseAmount(ctx context.Context, id string, amount 
 	return nil
 }
 
+func (bs *balanceStorage) DecreaseAmount(ctx context.Context, id string, amount int64) error {
+	sql, args, buildErr := bs.queryBuilder.
+		Update(bs.tableScheme).
+		Set("amount", fmt.Sprintf("amount + %d", amount)).
+		Where(sq.Eq{"id": id}).ToSql()
+	log.Info(fmt.Sprintf("table = %q ; sql = %q ; args = %q", bs.tableScheme, sql, args))
+	if buildErr != nil {
+		// TODO: add wrapping
+		// buildErr
+		return buildErr
+	}
+	if exec, execErr := bs.pool.Exec(ctx, sql, args...); execErr != nil {
+		// TODO: wrap
+		return execErr
+	} else if exec.RowsAffected() == 0 || !exec.Insert() {
+		// TODO: create err
+		return fmt.Errorf("balance was not decreased")
+	}
+	return nil
+}
+
 func (bs *balanceStorage) GetByID(ctx context.Context, id string) (*entity.Balance, error) {
 	sql, args, buildErr := bs.queryBuilder.
-		Select("id").
-		Columns("amount").
+		Select("id").Columns("amount").
 		From(bs.tableScheme).Where(sq.Eq{"id": id}).ToSql()
 	// TODO: add logging
 	log.Info(fmt.Sprintf("table = %q ; sql = %q ; args = %q", bs.tableScheme, sql, args))
 	if buildErr != nil {
 		// TODO: add wrapping
-		//buildErr
+		// buildErr
 		return nil, buildErr
 	}
 	var obj entity.Balance
@@ -66,6 +86,6 @@ func (bs *balanceStorage) GetByID(ctx context.Context, id string) (*entity.Balan
 		// TODO: wrap error
 		return nil, err
 	}
-	//bs.pool.Query()
+	// bs.pool.Query()
 	return &obj, nil
 }
