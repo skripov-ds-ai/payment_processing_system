82972503a390ff62362fa15d7d235d197cf5ee07
Denis Skripov <nizhikebinesi@gmail.com>
2023-01-27 19:31:20 +0600

    add date_time_updated to transaction, add updating datetime in pgx transaction storage

--- internal/domain/entity/transaction.go
+++ internal/domain/entity/transaction.go
@@ -25,10 +25,10 @@ const (
 // Transaction is entity to represent transaction in payment processing system
 // Please, do not use float32 for money operations in production!
 type Transaction struct {
-	ID            string            `json:"id"`
-	SourceID      *string           `json:"source_id"`
-	DestinationID *string           `json:"destination_id"`
-	Amount        float32           `json:"amount"`
+	ID              string            `json:"id"`
+	SourceID        *string           `json:"source_id"`
+	DestinationID   *string           `json:"destination_id"`
+	Amount          float32           `json:"amount"`
 	Type            TransactionType   `json:"type"`
 	DateTimeCreated time.Time         `json:"date_time_created"`
 	DateTimeUpdated time.Time         `json:"date_time_updated"`
