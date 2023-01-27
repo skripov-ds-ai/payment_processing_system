f5e51cc34d3dca19e9ae71848c54fe7969615df7
Denis Skripov <nizhikebinesi@gmail.com>
2023-01-27 19:08:27 +0600

    fix pgx storages

--- internal/adapters/db/relational/pgx/transaction.go
+++ internal/adapters/db/relational/pgx/transaction.go
@@ -51,7 +51,7 @@ func (ts *transactionStorage) UpdateStatusByID(ctx context.Context, id, status s
 	return nil
 }
 
-func (ts *transactionStorage) Create(ctx context.Context, transaction entity.Transaction) (string, error) {
+func (ts *transactionStorage) Create(ctx context.Context, transaction entity.Transaction) (*string, error) {
 	sql, args, buildErr := ts.queryBuilder.
 		Insert(ts.tableScheme).Columns(
 		"amount", "source_id", "destination_id",
@@ -68,13 +68,13 @@ func (ts *transactionStorage) Create(ctx context.Context, transaction entity.Tra
 	if buildErr != nil {
 		// TODO: add wrapping
 		// buildErr
-		return "", buildErr
+		return nil, buildErr
 	}
 	err := ts.pool.QueryRow(ctx, sql, args...).Scan(&transaction.ID)
 	if err != nil {
-		return transaction.ID, err
+		return &transaction.ID, err
 	}
-	return transaction.ID, nil
+	return &transaction.ID, nil
 }
 
 func (ts *transactionStorage) GetByID(ctx context.Context, id string) (*entity.Transaction, error) {
