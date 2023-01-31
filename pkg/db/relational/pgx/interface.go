95b92359b2de99fa2b53940663fd39d7e320062d
Denis Skripov <nizhikebinesi@gmail.com>
2023-02-01 01:29:09 +0600

    add interface

--- pkg/db/relational/pgx/interface.go
+++ pkg/db/relational/pgx/interface.go
@@ -1,1 +1,20 @@
 package pgx
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type PgxIface interface {
+	Begin(context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
+	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
+	QueryRow(context.Context, string, ...interface{}) pgx.Row
+	Ping(context.Context) error
+	Prepare(context.Context, string, string) (*pgconn.StatementDescription, error)
+	Deallocate(ctx context.Context, name string) error
+	PgConn() *pgconn.PgConn
+	Close()
+}
