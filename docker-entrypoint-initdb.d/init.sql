0d88af8d8651ad1f9ff46252ae9777cb97c00614
Denis Skripov <nizhikebinesi@gmail.com>
2023-02-01 05:32:26 +0600

    add indexes

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -2,6 +2,7 @@ CREATE TABLE balance (
     id BIGINT PRIMARY KEY,
     amount NUMERIC(20, 2) NOT NULL CONSTRAINT non_negative_amount CHECK (amount >= 0)
 );
+CREATE INDEX cover_balance ON balance(id) INCLUDE (amount);
 CREATE TABLE transaction (
     id BIGSERIAL PRIMARY KEY,
     source_id BIGINT,
@@ -17,3 +18,11 @@ CREATE TABLE transaction (
     CONSTRAINT fk_destination_id FOREIGN KEY(destination_id) REFERENCES balance(id),
     CONSTRAINT valid_status_source_destination CHECK (ttype = 'increasing' AND source_id IS NULL AND destination_id IS NOT NULL OR (ttype = 'decreasing' OR ttype = 'payment') AND source_id IS NOT NULL AND destination_id IS NULL OR ttype = 'transfer' AND source_id IS NOT NULL AND destination_id IS NOT NULL)
 );
+-- CREATE INDEX source_transaction ON transaction(source_id);
+CREATE INDEX source_created_transaction ON transaction(source_id, date_time_created);
+CREATE INDEX source_updated_transaction ON transaction(source_id, date_time_updated);
+-- CREATE INDEX destination_transaction ON transaction(destination_id);
+CREATE INDEX destination_created_transaction ON transaction(destination_id, date_time_created);
+CREATE INDEX destination_updated_transaction ON transaction(destination_id, date_time_updated);
+CREATE INDEX pay_for_service_ttype_created_transaction ON transaction(date_time_created) WHERE ttype = 'payment';
+CREATE INDEX pay_for_service_ttype_updated_transaction ON transaction(date_time_updated) WHERE ttype = 'payment';
