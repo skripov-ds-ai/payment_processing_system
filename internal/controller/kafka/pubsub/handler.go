16e6392e4ff82d5747dbe335784c296596d70bf1
Denis Skripov <nizhikebinesi@gmail.com>
2023-02-05 06:40:31 +0600

    simplify kafka handler, add constructor

--- internal/controller/kafka/pubsub/handler.go
+++ internal/controller/kafka/pubsub/handler.go
@@ -22,6 +22,10 @@ type ConsumerHandler struct {
 	verifier VerifierUseCase
 }
 
+func NewConsumerHandler(applier ApplierUseCase, verifier VerifierUseCase) *ConsumerHandler {
+	return &ConsumerHandler{applier: applier, verifier: verifier}
+}
+
 func (c *ConsumerHandler) Setup(sarama.ConsumerGroupSession) error {
 	return nil
 }
@@ -39,50 +43,43 @@ func (c *ConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, clai
 			if !ok {
 				return nil
 			}
-			// TODO: add constants
-			ctx := session.Context()
-			switch message.Topic {
-			case "apply":
-				var event messages.ApplyTransactionEvent
-				err := jsoniter.Unmarshal(message.Value, &event)
-				if err != nil {
-					return err
-				}
-				err = c.applier.ApplyTransaction(ctx, event.Transaction)
-				if err != nil {
-					return err
-				}
-			case "complete":
-				var event messages.CompleteTransactionEvent
-				err := jsoniter.Unmarshal(message.Value, &event)
-				if err != nil {
-					return err
-				}
-				err = c.verifier.CompleteTransactionByID(ctx, event.TransactionID)
-				if err != nil {
-					return err
-				}
-			case "cancel":
-				var event messages.CancelTransactionEvent
-				err := jsoniter.Unmarshal(message.Value, &event)
-				if err != nil {
-					return err
-				}
-				err = c.verifier.CancelTransactionByID(ctx, event.TransactionID)
-				if err != nil {
-					return err
-				}
+			err := c.processMessage(session.Context(), message)
+			if err != nil {
+				// TODO: log err
+				continue
 			}
-			//var (
-			//	sender    = string(message.Value)
-			//	offset    = message.Offset
-			//	partition = message.Partition
-			//)
-
-			//log.Printf("offset: %d, partition: %d, value: %v", offset, partition, sender)
-			//metric.IncMessagesConsumed(sender, offset, partition)
 			// TODO
 			session.MarkMessage(message, "")
 		}
 	}
+}
+
+func (c *ConsumerHandler) processMessage(ctx context.Context, message *sarama.ConsumerMessage) (err error) {
+	switch message.Topic {
+	case "apply":
+		var event messages.ApplyTransactionEvent
+		err = jsoniter.Unmarshal(message.Value, &event)
+		if err != nil {
+			return err
+		}
+		err = c.applier.ApplyTransaction(ctx, event.Transaction)
+	case "complete":
+		var event messages.CompleteTransactionEvent
+		err = jsoniter.Unmarshal(message.Value, &event)
+		if err != nil {
+			return err
+		}
+		err = c.verifier.CompleteTransactionByID(ctx, event.TransactionID)
+	case "cancel":
+		var event messages.CancelTransactionEvent
+		err = jsoniter.Unmarshal(message.Value, &event)
+		if err != nil {
+			return err
+		}
+		err = c.verifier.CancelTransactionByID(ctx, event.TransactionID)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
